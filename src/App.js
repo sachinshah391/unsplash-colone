d1d12e2afbbad40da4ed9cd2cf5c3dc9f7c6667a
Sachin Shah <sachinshah391@gmail.com>
2022-02-08 23:44:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+
+  const [value, setvalue] = useState("");
+  const [imgresult, setimgresult] = useState([]);   
+
+  const fetchData= async()=>{
+
+    const result=await fetch(`https://api.unsplash.com/search/photos?client_id=Oao-1ry9nWlaBUiADQzwqNMPx5rLUrHytv4SfVhu9-Q&query=${value}&orientation=squarish`)
+    const json=await result.json();
+    //console.log(json);    
+    setimgresult(json.results)
+  }
+
+  const handleChange=(e)=>{
+    return setvalue(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mydiv">
+        <input type="text" placeholder='Search..' value={value} onChange={handleChange} />
+        <button onClick={fetchData}><strong>Send</strong></button>
+      </div>
+
+
+      <div className="gallery">
+        {
+          imgresult.map((items)=>{
+            return <img src={items.urls.regular} key={items.id} alt={value}></img>
+          })
+        }
+      </div>
+    </>
+   
   );
 }
 
